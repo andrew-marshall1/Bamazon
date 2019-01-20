cc179fd4d77df21a56607ec14da871a031eb7c8b
andrew-marshall1 <43158531+andrew-marshall1@users.noreply.github.com>
2019-01-20 11:36:58 -0700

    Add files via upload

---
+++ customer.js
@@ -0,0 +1,109 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Archer813!",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    start();
+});
+
+function start() {
+    console.log("\n===================="
+        + "\nFeel free to browse!"
+        + "\n====================\n")
+
+    connection.query("SELECT * FROM products", function(err, results) {
+
+        for (var i = 0; i < results.length; i++) {
+            console.log("Product Name: " + results[i].product_name 
+            + "\nDepartment Name: " + results[i].department_name 
+            + "\nPrice: $" + results[i].price 
+            + "\nItem ID: " + results[i].item_id
+            + "\n\n===============================================\n");
+        };
+
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function() {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].product_name);
+                        };
+                        return choiceArray;
+                    },
+                    message: "What would you like to buy?"
+                },
+                {
+                    name: "quantity",
+                    type: "input",
+                    message: "How many would you like to purchase?"
+                }
+            ])
+            .then(function(answer) {
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].product_name === answer.choice) {
+                        chosenItem = results[i];
+                    };
+                };
+                if (chosenItem.stock_quantity >= answer.quantity) {
+                    var newQuantity = parseInt(chosenItem.stock_quantity - answer.quantity);
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: newQuantity
+                            },
+                            {
+                                item_id: chosenItem.item_id
+                            }
+                        ],
+                        function(error) {
+                            if (error) throw err;
+                        },
+                        console.log("\nThank you for your purchase! Your total is $" + chosenItem.price * answer.quantity + ".\n")
+                    )
+                    continueShopping();
+                } else {
+                    console.log("Sorry, we don't have that many in stock.")
+                    continueShopping();
+                };
+            });
+    });
+};
+
+function continueShopping() {
+    inquirer
+        .prompt({
+            name: "continue",
+            type: "rawlist",
+            choices: [
+                "Yes",
+                "No"
+            ],
+            message: "Would you like to continue shopping?"
+        })
+        .then(function(answer) {
+            console.log(answer)
+            if (answer.continue === "Yes") {
+                start();
+            } else {
+                console.log("\n====================================" + 
+                "\nThank you for shopping with Bamazon!" +
+                "\n====================================\n" 
+
+                );
+                connection.end();
+            }
+        })
+}
---
+++ seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Nike Hooded Sweatshirt", "Apparel", 270, 10),
+("Remington Hunting Knife", "Outdoors", 45, 15),
+("Adidas Track Jacket", "Apparel", 220, 10),
+("Nike Max Flight Shoes", "Apparel", 15, 25),
+("XBox 360 Console", "Electronics", 150, 10),
+("Madden 2018 Video Game", "Games", 80, 10),
+("Vizio 42 Inch TV", "Outdoors", 425, 5),
+("Black & Decker Push Lawnmower", "Outdoors", 250, 5),
+("Nike Sweatpants", "Apparel", 50, 10),
+("Apple Watch", "Apparel", 140, 10);
